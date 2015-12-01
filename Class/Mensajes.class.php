77fb24d8b3216b9b858015a260c29a2995c2e7aa
eguaman1990 <es.aguaman@gmail.com>
2015-12-01 10:34:22 -0300

    Merge origin/master

---
+++ Class/Mensajes.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Mensajes
+ *
+ * @author Edwin
+ */
+class Mensajes {
+    //put your code here
+    const REGISTRO_AGREGADO="Registro Ingresado Exitosamente";
+    const REGISTRO_ACTUALIZADO="Registro Actualizado Exitosamente";
+    const REGISTRO_ELIMINADO="Registro Eliminado Exitosamente";
+}
--- Controller/usuarioDeleteController.php
+++ Controller/usuarioDeleteController.php
@@ -2,28 +2,57 @@
 
 require_once "../class/BD.class.php";
 require_once "../class/Tablas.class.php";
+require_once "../class/Mensajes.class.php";
 
-$respuesta=  "";
-$estado="";
-$mensaje="";
-if(isset($_REQUEST["accion"])){
-  $accion=$_REQUEST["accion"];
-}else{
-  $accion="";
+$respuesta = "";
+$estado = "";
+$mensaje = "";
+if (isset($_REQUEST["accion"])) {
+    $accion = $_REQUEST["accion"];
+} else {
+    $accion = "";
 }
 
-if(isset($_REQUEST["id_persona"])){
-  $id_persona=$_REQUEST["id_persona"];
-}else{
-  $id_persona="";
+if (isset($_REQUEST["id_persona"])) {
+    $id_persona = $_REQUEST["id_persona"];
+} else {
+    $id_persona = "";
 }
 
-if($accion=="eliminar"){
-    
+if ($accion == "eliminar") {
+    $bd = new BD();
+    $bd->beginTransaction();
+    try {
+        $parametros = array("id_persona" => $id_persona);
+        $res = $bd->delete(tablas::USUARIOS, $parametros);
+        if ($bd->myException->getEstado() == 0) {
+            $res1= $bd->delete(tablas::PERSONAS,$parametros);
+            if ($bd->myException->getEstado() == 0) {
+                $estado="ok";
+                $mensaje=Mensajes::REGISTRO_ELIMINADO;
+            }else{
+                $estado = "error";
+                $mensaje = $bd->myException->getMensaje();
+            }
+        } else {
+            $estado = "error";
+            $mensaje = $bd->myException->getMensaje();
+        }
+    } catch (Exception $e) {
+        $bd->rollBack();
+        $estado = "error";
+        $mensaje = $e->getMensaje();
+    } catch (Exception $e) {
+        $bd->rollBack();
+        $estado = "error";
+        $mensaje = $e->getMensaje();
+    }
+    $bd = NULL;
+    $respuesta[] = array("estado" => $estado, "mensaje" => $mensaje);
 }
 
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Fecha en el pasado
 header("Content-type: application/json");
-$respuesta=json_encode($respuesta);
+$respuesta = json_encode($respuesta);
 echo $respuesta;
